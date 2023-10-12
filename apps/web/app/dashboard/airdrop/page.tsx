6484bf096da60c689fdcdaef1175bb91d3a7d5b4
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-10-12 11:54:53 +0530

    feat: show balance in airdrop page

--- apps/web/app/dashboard/airdrop/page.tsx
+++ apps/web/app/dashboard/airdrop/page.tsx
@@ -1,13 +1,28 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import {
+  getAccount,
+  getAssociatedTokenAddress,
+  getAssociatedTokenAddressSync,
+  TokenAccountNotFoundError,
+  TokenInvalidAccountOwnerError,
+} from "@solana/spl-token"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { PublicKey } from "@solana/web3.js"
 import axios from "axios"
 import { toast } from "sonner"
 
+import { TOKEN_MINT } from "@/lib/constants"
 import { Button } from "@/components/ui/button"
 
 const AirdropPage = () => {
   const [isAirdropping, setIsAirdropping] = useState(false)
+  const [balance, setBalance] = useState(0)
+  const [isLoadingBalance, setIsLoadingBalance] = useState(false)
+
+  const { publicKey } = useWallet()
+  const { connection } = useConnection()
 
   const airdrop = async () => {
     setIsAirdropping(true)
@@ -22,9 +37,45 @@ const AirdropPage = () => {
     }
   }
 
+  useEffect(() => {
+    ;(async () => {
+      if (!publicKey) {
+        return
+      }
+
+      setIsLoadingBalance(true)
+      try {
+        const ata = getAssociatedTokenAddressSync(
+          new PublicKey(TOKEN_MINT),
+          publicKey
+        )
+
+        console.log(ata)
+
+        const accountData = await getAccount(connection, ata)
+
+        setBalance(Number(accountData.amount.toString()))
+        setIsLoadingBalance(false)
+      } catch (error) {
+        if (
+          error instanceof TokenAccountNotFoundError ||
+          error instanceof TokenInvalidAccountOwnerError
+        ) {
+          setBalance(0)
+        } else {
+          console.error(error)
+          toast.error("Error fetching balance")
+          setIsLoadingBalance(false)
+        }
+      }
+    })()
+  }, [isAirdropping, publicKey])
+
   return (
     <>
       <h1 className="text-3xl font-bold">Airdrop 10000 VAX Tokens</h1>
+
+      <p>Balance: {isLoadingBalance ? "Loading..." : balance}</p>
 
       <Button onClick={airdrop} isLoading={isAirdropping} className="mt-4">
         Airdrop
